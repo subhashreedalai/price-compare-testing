0f8e324cfb89fb87d4f890bac1fc53f34b24c2a5
subhashreedalai <60112514+subhashreedalai@users.noreply.github.com>
2020-01-21 00:51:59 +0530

    Added search page test cases

---
+++ Searchphonetest.java
@@ -0,0 +1,23 @@
+package testcases;
+import java.sql.Driver;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import base.Driverinstance;
+import pages.Searchphone;
+
+public class Searchphonetest extends Driverinstance 
+	{
+		@Test
+	public void searchphone()throws Exception{
+		Searchphone phn=new Searchphone();
+		PageFactory.initElements(driver,phn);
+		phn.searchbox();
+		phn.searchicon();
+		phn.clickphone();
+		phn.getprice();
+		phn.flipkart();
+	}
+
+}
