38fa1172e1f31ab36c322c48abb036259fcd98c3
subhashreedalai <60112514+subhashreedalai@users.noreply.github.com>
2020-01-21 12:16:00 +0530

    Update Searchphone.java

--- Searchphone.java
+++ Searchphone.java
@@ -8,11 +8,14 @@ import org.openqa.selenium.support.FindBy;
 
 public class Searchphone {
 WebDriver driver;
+String s= "";
 @FindBy(id="twotabsearchtextbox")WebElement searchbox;
 @FindBy(xpath="//input[@type='submit']")WebElement searchicon;
 @FindBy(xpath="//span[@class='a-size-medium a-color-base a-text-normal']")WebElement clickphone;
-@FindBy(xpath="//span[@class='a-price-whole']")WebElement getprice;
-
+@FindBy(xpath="//span[@class='a-price-whole']")WebElement getamazonprice;
+@FindBy(xpath="//input[@class='LM6RPg']")WebElement flipkartsearchbox;
+@FindBy(xpath="//svg[@viewBox='0 0 17 18']")WebElement clicksearchbtn;
+@FindBy(xpath="//div[@class='_1vC4OE _2rQ-NK']")WebElement getflipkartprice;
 public void searchbox() throws Exception
 {
 	searchbox.sendKeys("iphone XR(64GB)-Yellow");
@@ -26,23 +29,32 @@ public void clickphone()
 {
 	clickphone.click();
 	}
-public void getprice() throws Exception
+public void getamazonprice() throws Exception
 {
-	String s= getprice.getText();
+	String s= getamazonprice.getText();
 			System.out.println(s);
 			Thread.sleep(3000);
 	
 	
 }
-public void flipkart() throws Exception
+public void flipkart()
 {
-	
-	driver.get("https://www.flipkart.com/");
-	WebElement w1 = driver.findElement(By.xpath("//input[@class='LM6RPg']"));
-	w1.sendKeys("iphone XR(64GB)-Yellow");
-	w1.sendKeys(Keys.ENTER);
-	Thread.sleep(30000);
+	driver.navigate().to("https//:www.flipkart.com");
+}
+public void flipkartsearchbox()
+{
+	flipkartsearchbox.sendKeys("iphone XR(64GB)-Yellow");
+		
+}
+public void clicksearchbtn(){
+	clicksearchbtn.click();
+}
+public void getflipkartprice()throws Exception
+{
+	String s1=getflipkartprice.getText();
+	System.out.println(s1);
+	Thread.sleep(3000);
+	s1.compareTo(s);
 	
 }
-
 }
